b494d7830b0a878b2504e55827d18f4a0327a179
ARYANAayu <gajjararyan63@gmail.com>
2023-11-07 00:13:44 +0530

    MaximumProductOfThreeNumbers.cpp
    Problem Number 628

--- MaximumProductofThreeNumbers.cpp
+++ MaximumProductofThreeNumbers.cpp
@@ -2,6 +2,9 @@ class Solution {
 public:
     int maximumProduct(vector<int>& A) {
         sort(begin(A), end(A));
-        return A.back() * max(A[A.size() - 2] * A[A.size() - 3], A[0] * A[1]);
+        int n = A.size();
+        int product1 = A[n - 1] * A[n - 2] * A[n - 3]; 
+        int product2 = A[0] * A[1] * A[n - 1];        
+        return max(product1, product2);
     }
 };
