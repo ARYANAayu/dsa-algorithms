fb0663d39b3b43f4cf9baacbb5b461f7009275f2
ARYANAayu <92173539+ARYANAayu@users.noreply.github.com>
2024-01-04 15:37:18 +0530

    Problem Number 724

---
+++ FindPivotIndex.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        if (nums.empty()) return -1;
+        int sum = accumulate(nums.begin(), nums.end(), 0) - nums[0];
+        int left = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (left == sum) return i;
+            left += nums[i];
+            if (i + 1 < nums.size()) sum -= nums[i + 1];
+        }
+        return -1;
+    }
+};
