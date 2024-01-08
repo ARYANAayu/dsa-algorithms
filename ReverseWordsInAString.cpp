20503c28302bd22bf57f9a0c354334e07592f5ab
ARYANAayu <92173539+ARYANAayu@users.noreply.github.com>
2024-01-08 15:11:29 +0530

    Problem Number 151

---
+++ ReverseWordsInAString.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string reverseWords(string &s) {
+        int i = 0, j = 0;
+        while (j < s.size()) {
+            while (j < s.size() && s[j] == ' ') ++j;
+            if (i != 0 && j != s.size()) s[i++] = ' ';
+            while (j < s.size() && s[j] != ' ') s[i++] = s[j++];
+        }
+        s.erase(i);
+        i = j = 0;
+        while (i < s.size()) {
+            j = i;
+            while (j < s.size() && s[j] != ' ') ++j;
+            reverse(s.begin() + i, s.begin() + j);
+            i = j + 1;
+        }
+        reverse(s.begin(), s.end());
+        
+        // Return the modified string
+        return s;
+    }
+};
